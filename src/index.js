af766673fc9aada70c7b07fd9840dc647b783276
Matt Luker <jamesmattluker@gmail.com>
2021-11-15 12:56:50 -0700

    add zipcode and fahrenheit conversion and extra API calls

--- src/index.js
+++ src/index.js
@@ -16,14 +16,75 @@ $(document).ready(function() {
         const response = JSON.parse(this.responseText);
         getElements(response);
       }
-    }
+    };
 
     request.open("GET", url, true);
     request.send();
 
     function getElements(response) {
       $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
-      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+      $('.showTemp').text(`The temperature is ${Math.floor((response.main.temp - 273.15) * (9/5) + 32)} degrees Fahrenheit.`);
+      $('.showDescription').text(`It looks like the weather in ${city} is ${response.weather[0].description}`);
+      $('.windSpeed').text(`The wind speed in ${city} is ${response.wind.speed} mph`);
+      $('.feelsLike').text(`The feels like temperature in ${city} is ${Math.floor((response.main.feels_like - 273.15) * (9/5) + 32)} degrees Fahrenheit.`);      
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('#weatherZipCode').click(function() {
+    const zipCode = $('#zipCode').val();
+    $('#zipCode').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}&appid=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${zipCode} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature is ${Math.floor((response.main.temp - 273.15) * (9/5) + 32)} degrees Fahrenheit.`);
+      $('.showDescription').text(`It looks like the weather in ${zipCode} is ${response.weather[0].description}`);
+      $('.windSpeed').text(`The wind speed in ${zipCode} is ${response.wind.speed} mph`);
+      $('.feelsLike').text(`The feels like temperature in ${zipCode} is ${Math.floor((response.main.feels_like - 273.15) * (9/5) + 32)} degrees Fahrenheit.`);      
+    }
+  });
+});
+
+$(document).ready(function() {
+  $('#weatherLatAndLong').click(function() {
+    const latitude = $('#latitude').val();
+    const longitude = $('#longitude').val();
+    $('#latitude').val("");
+    $('#longitude').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity is ${response.daily[2].humidity}%`);
+      // $('.showTemp').text(`The temperature is ${Math.floor((response.main.temp - 273.15) * (9/5) + 32)} degrees Fahrenheit.`);
+      // $('.showDescription').text(`It looks like the weather in ${zipCode} is ${response.weather[0].description}`);
+      // $('.windSpeed').text(`The wind speed in ${zipCode} is ${response.wind.speed} mph`);
+      // $('.feelsLike').text(`The feels like temperature in ${zipCode} is ${Math.floor((response.main.feels_like - 273.15) * (9/5) + 32)} degrees Fahrenheit.`);      
     }
   });
 });
